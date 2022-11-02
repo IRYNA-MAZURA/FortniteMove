4600a918b4c44860f11d019f0a9ad1c628cdade7
zephsie <zephiezephs@gmail.com>
2022-11-02 17:51:15 +0300

    final fixes

--- js.js
+++ js.js
@@ -1,41 +1,41 @@
 document.querySelector('#btn1').addEventListener('click', () => {
     let buttons = document.getElementsByClassName("button");
-    for (i = 0; i < buttons.length; i++) {
+    for (let i = 0; i < buttons.length; i++) {
         buttons[i].style.borderTop = '2px solid transparent';
     }
     buttons[0].style.borderTop = '2px solid white';
 })
 document.querySelector('#btn2').addEventListener('click', () => {
     let buttons = document.getElementsByClassName("button");
-    for (i = 0; i < buttons.length; i++) {
+    for (let i = 0; i < buttons.length; i++) {
         buttons[i].style.borderTop = '2px solid transparent';
     }
     buttons[1].style.borderTop = '2px solid white';
 })
 document.querySelector('#btn3').addEventListener('click', () => {
     let buttons = document.getElementsByClassName("button");
-    for (i = 0; i < buttons.length; i++) {
+    for (let i = 0; i < buttons.length; i++) {
         buttons[i].style.borderTop = '2px solid transparent';
     }
     buttons[2].style.borderTop = '2px solid white';
 })
 document.querySelector('#btn4').addEventListener('click', () => {
     let buttons = document.getElementsByClassName("button");
-    for (i = 0; i < buttons.length; i++) {
+    for (let i = 0; i < buttons.length; i++) {
         buttons[i].style.borderTop = '2px solid transparent';
     }
     buttons[3].style.borderTop = '2px solid white';
 })
 document.querySelector('#btn5').addEventListener('click', () => {
     let buttons = document.getElementsByClassName("button");
-    for (i = 0; i < buttons.length; i++) {
+    for (let i = 0; i < buttons.length; i++) {
         buttons[i].style.borderTop = '2px solid transparent';
     }
     buttons[4].style.borderTop = '2px solid white';
 })
 document.querySelector('#btn6').addEventListener('click', () => {
     let buttons = document.getElementsByClassName("button");
-    for (i = 0; i < buttons.length; i++) {
+    for (let i = 0; i < buttons.length; i++) {
         buttons[i].style.borderTop = '2px solid transparent';
     }
     buttons[5].style.borderTop = '2px solid white';
